7cf73b94625947e9be5b01ea5344a5fbac5f76be
diptnc <chakrabortyd943@gmail.com>
2022-04-08 14:19:38 +0530

    lol

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '8cb362b10da97281814957291cc582d51db8424a',
+    'name' => 'qoddiapps/getting-started-with-php',
+  ),
+  'versions' => 
+  array (
+    'alrik11es/cowsayphp' => 
+    array (
+      'pretty_version' => '1.2.0',
+      'version' => '1.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '080dc10c8f5420fef35163569293601f17124eeb',
+    ),
+    'qoddiapps/getting-started-with-php' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8cb362b10da97281814957291cc582d51db8424a',
+    ),
+  ),
+);
